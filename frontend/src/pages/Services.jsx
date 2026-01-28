2536eed7f77f5c14e7681e102bf5a7aee53da84e
Jaswanth Simha <jaswanthsimha2004@gmail.com>
2026-01-29 04:19:31 +0530

    this is code

--- frontend/src/pages/Services.jsx
+++ frontend/src/pages/Services.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import { Helmet } from "react-helmet-async";
-
 import ServiceHero from "../components/Services/ServiceHero.jsx";
 import ServiceGrid from "../components/Services/ServiceGrid.jsx";
 import Advantage from "../components/Services/Advantage.jsx";
